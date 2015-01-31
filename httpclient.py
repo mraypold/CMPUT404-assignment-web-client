7447e0d07da707ee58898be09797c05621352c87
Michael Raypold <mraypold@users.noreply.github.com>
2015-01-31 14:51:27 -0700

    Functionally complete - testing

--- httpclient.py
+++ httpclient.py
@@ -123,29 +123,43 @@ class HTTPClient(object):
     def GET(self, url, args=None):
         code = 500
         body = ""
-        parsed = urlparse.urlsplit(url)
-        # sock = self.connect(parse.get('path', '/'), 80)
-        # self.poll_sockets(sock, 'GET / HTTP/1.1\r\n\r\n')
+
+        code, body = self.build_and_send(url, args, "GET")
+
+        print 'debug'
+        print code
+        print 'debug'
+
         return HTTPRequest(code, body)
 
     def POST(self, url, args=None):
         code = 500
         body = ""
 
-        # Encoded message to send in POST
+        code, body = self.build_and_send(url, args, "POST")
+
+        return HTTPRequest(code, body)
+
+    def build_and_send(self, url, args=None, rtype="GET"):
+        """
+        Builds a POST/GET request and sends/receives a response.
+        Takes a url, arguments (python dictionary) and request type.
+        Returns the HTTP/1.1 status code and message body.
+        """
+        # Encoded message to send in body
         encoded = urllib.urlencode(args) if args else ''
 
-        parsed =  urlparse.urlsplit(url)
-        header = HTTPHeader(parsed.path, parsed.hostname, "POST", encoded)
+        parsed = urlparse.urlsplit(url)
+        header = HTTPHeader(parsed.path, parsed.hostname, rtype, encoded)
         request = header.get_header()
+
+        print request
 
         port = parsed.port if parsed.port else 80
         sock = self.connect(parsed.hostname, port)
 
         result = self.poll_sockets(sock, request)
-        code, body = self.parse_result(result)
-
-        return HTTPRequest(code, body)
+        return self.parse_result(result)
 
     def parse_result(self, result):
         """
@@ -153,7 +167,7 @@ class HTTPClient(object):
         """
         try:
             head, body = re.split(r'\r\n\r\n', result, 1)
-            code = head.split()[1]
+            code = int(head.split()[1])
         except:
             code = 500
             body = ''
@@ -168,16 +182,10 @@ class HTTPClient(object):
 if __name__ == "__main__":
     client = HTTPClient()
     command = "GET"
-    # for testing right now
-    request = client.POST(sys.argv[2], {'a':'foo', 'b':'bar'})
-    print request.code
-    print request.body
-    # if (len(sys.argv) <= 1):
-    #     help()
-    #     sys.exit(1)
-    # elif (len(sys.argv) == 3):
-    #     print client.command( sys.argv[1], sys.argv[2] ) # still not convnced
-    #     print '2'
-    # else:
-    #     print client.command( command, sys.argv[1] ) # Still not convinced
-    #     print '3'
+    if (len(sys.argv) <= 1):
+        help()
+        sys.exit(1)
+    elif (len(sys.argv) == 3):
+        print client.command( sys.argv[1], sys.argv[2] )
+    else:
+        print client.command( command, sys.argv[1] )    
