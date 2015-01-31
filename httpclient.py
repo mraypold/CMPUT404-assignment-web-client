b07ae0fc069065dd76363ce5a744dd1ad809c7eb
Michael Raypold <mraypold@users.noreply.github.com>
2015-01-31 13:19:22 -0700

    Initial POST working

--- httpclient.py
+++ httpclient.py
@@ -24,8 +24,8 @@ import socket
 import re
 # you may use urllib to encode data appropriately
 import urllib
-
-VERBOSE = True
+import urlparse
+import mimetypes
 
 def help():
     print "httpclient.py [GET/POST] [URL]\n"
@@ -35,8 +35,27 @@ class HTTPRequest(object):
         self.code = code
         self.body = body
 
+class HTTPHeader(object):
+    def __init__(self, path, host, method="GET", message=""):
+
+        self.request = "%s %s HTTP/1.1\r\n" %(method, path)
+        self.request += "Host: %s\r\n" %host
+        self.request += "User-Agent: Python\r\n"
+        self.request += "Accept: */*\r\n"
+
+        if method == "GET":
+            self.request += "Content-type: %s\r\n" %(mimetypes.guess_type(path)[0]) # Test THIS TODO
+        else:
+            self.request += "Content-type: application/x-www-form-urlencoded\r\n"
+
+        self.request += "Connection: close\r\n"
+        self.request += "Content-Length: %s\r\n\r\n" %len(message)
+        self.request += message
+
+    def get_header(self):
+        return self.request.encode('utf-8')
+
 class HTTPClient(object):
-    #def get_host_port(self,url):
 
     def connect(self, host, port):
         sock = self.init_socket()
@@ -68,38 +87,28 @@ class HTTPClient(object):
         sockout = [sock]
         sockexc = []
 
-        while True:
+        result = ''
+
+        while not result:
             readable, writable, exceptions = select.select(sockin, sockout, sockexc)
 
             for s in readable:
-                if VERBOSE: print 'Have readable socket'
                 sockin.pop()
-                print(self.recvall(s))
-                # print s.recv(4096)
-                print('wtf is happening')
-                sys.exit()
+                result = self.recvall(s)
+                sock.close()
 
             for s in writable:
-                if VERBOSE: print 'Have writable socket'
                 s.sendall(request)
                 sockin.append(sockout.pop())
 
             for s in exceptions:
-                if VERBOSE: print 'Have exception'
+                print 'Exception'
                 sys.exit()
 
-        return None
-
-    def get_code(self, data):
-        return None
-
-    def get_headers(self,data):
-        return None
-
-    def get_body(self, data):
-        return None
+        return result
 
     # read everything from the socket
+    # TODO add a timeout if the connection doesn't close
     def recvall(self, sock):
         buffer = bytearray()
         done = False
@@ -109,22 +118,46 @@ class HTTPClient(object):
                 buffer.extend(part)
             else:
                 done = not part
-            print str(buffer)
         return str(buffer)
 
     def GET(self, url, args=None):
-        if VERBOSE: print 'GETing %s' %url
         code = 500
         body = ""
-        sock = self.connect(url, 80)
-        self.poll_sockets(sock, 'GET / HTTP/1.1\r\n\r\n')
+        parsed = urlparse.urlsplit(url)
+        # sock = self.connect(parse.get('path', '/'), 80)
+        # self.poll_sockets(sock, 'GET / HTTP/1.1\r\n\r\n')
         return HTTPRequest(code, body)
 
     def POST(self, url, args=None):
-        if VERBOSE: print 'POSTing %s' %url
         code = 500
         body = ""
+
+        # Encoded message to send in POST
+        encoded = urllib.urlencode(args) if args else ''
+
+        parsed =  urlparse.urlsplit(url)
+        header = HTTPHeader(parsed.path, parsed.hostname, "POST", encoded)
+        request = header.get_header()
+
+        port = parsed.port if parsed.port else 80
+        sock = self.connect(parsed.hostname, port)
+
+        result = self.poll_sockets(sock, request)
+        code, body = self.parse_result(result)
+
         return HTTPRequest(code, body)
+
+    def parse_result(self, result):
+        """
+        Takes an HTTP/1.1 response and returns a status code and message body
+        """
+        try:
+            head, body = re.split(r'\r\n\r\n', result, 1)
+            code = head.split()[1]
+        except:
+            code = 500
+            body = ''
+        return code, body
 
     def command(self, url, command="GET", args=None):
         if (command == "POST"):
@@ -135,10 +168,16 @@ class HTTPClient(object):
 if __name__ == "__main__":
     client = HTTPClient()
     command = "GET"
-    if (len(sys.argv) <= 1):
-        help()
-        sys.exit(1)
-    elif (len(sys.argv) == 3):
-        print client.command( sys.argv[2], sys.argv[1] )
-    else:
-        print client.command( sys.argv[2], command )
+    # for testing right now
+    request = client.POST(sys.argv[2], {'a':'foo', 'b':'bar'})
+    print request.code
+    print request.body
+    # if (len(sys.argv) <= 1):
+    #     help()
+    #     sys.exit(1)
+    # elif (len(sys.argv) == 3):
+    #     print client.command( sys.argv[1], sys.argv[2] ) # still not convnced
+    #     print '2'
+    # else:
+    #     print client.command( command, sys.argv[1] ) # Still not convinced
+    #     print '3'
